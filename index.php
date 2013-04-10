c498000269206df67e00aefef5bfa790f49dd9f2
Andrew Arnautov <arni@dream-clouds.com>
2013-04-10 07:30:49 +0100

    Changed to OOP Code refactoring  and commenting (total time +40 min)

--- index.php
+++ index.php
@@ -1,50 +1,77 @@
 <?php
 
-// ensure file download dialog
-header("Content-type: text/csv");  
-header("Cache-Control: no-store, no-cache");  
-header('Content-Disposition: attachment; filename="filename.csv"'); 
+$report = new Report(date("j", time()), date("n", time()), date("Y", time()), 15);
+$report->display();
 
-// set variables for later reuse
-$year = date("Y", time());
-$bonusDay = 15;
+/**
+ * @author     Andrei Arnautov <arni@dream-clouds.com>
+ */
+class Report {
 
-$currMonth = date("n", time());
-$currDay = date("j", time());
+    private $array;
 
-// go through remaining month of the year and get
-for($month = $currMonth; $month<=12; $month++){
-    
-    // create timestamp for later reuse
-    $time = mktime(0, 0, 0, $month, $bonusDay, $year);
-    
-    // add month name
-    $array[$month][1] = date("F", $time);
-    
-    // add last working day of the month
-    if ($checkWeekEnd = date("N", strtotime("last day of this month", $time)) - 5 > 0) $array[$month][2] = date("j", strtotime("last Friday", strtotime("last day of this month", $time)));
-    else $array[$month][2] = date("j", strtotime("last day of this month", $time));
-    
-    // add bonus day
-    if ($checkWeekEnd = date("N", $time) - 5 > 0) $array[$month][3] = date("j", strtotime("Wednesday", $time));
-    else $array[$month][3] = date("j", $time);
-    
-    // remove salary dates if they are before current day
-    if($month == $currMonth) {
-        if($array[$month][2]<$currDay && $array[$month][3]<$currDay) unset ($array[$month]);
-        else{
-            if($array[$month][2]<$currDay) $array[$month][2] = NULL;
-            if($array[$month][3]<$currDay) $array[$month][3] = NULL;
-        }            
+    /**
+     * Generates salary dates for remaining year
+     * 
+     * @param int $currDay number from 1 to 31
+     * @param int $currMonth number from 1 to 12
+     * @param int $currYear number like 2013
+     */
+    function __construct($currDay = 30, $currMonth = 6, $currYear = 2013, $bonusDay = 15) {
+
+        // go through remaining month of the year and get
+        for ($month = $currMonth; $month <= 12; $month++) {
+
+            // create timestamp for later reuse
+            $time = mktime(0, 0, 0, $month, $bonusDay, $currYear);
+
+            // add month name
+            $this->array[$month][1] = date("F", $time);
+
+            // add last working day of the month
+            if ($checkWeekEnd = date("N", strtotime("last day of this month", $time)) - 5 > 0)
+                $this->array[$month][2] = date("j", strtotime("last Friday", strtotime("last day of this month", $time)));
+            else
+                $this->array[$month][2] = date("j", strtotime("last day of this month", $time));
+
+            // add bonus day
+            if ($checkWeekEnd = date("N", $time) - 5 > 0)
+                $this->array[$month][3] = date("j", strtotime("Wednesday", $time));
+            else
+                $this->array[$month][3] = date("j", $time);
+
+            // remove salary dates if they are before current day
+            if ($month == $currMonth) {
+                if ($this->array[$month][2] < $currDay && $this->array[$month][3] < $currDay)
+                    unset($this->array[$month]);
+                else {
+                    if ($this->array[$month][2] < $currDay)
+                        $this->array[$month][2] = NULL;
+                    if ($this->array[$month][3] < $currDay)
+                        $this->array[$month][3] = NULL;
+                }
+            }
+        }
     }
+
+    /**
+     * Exports salary dates to CSV file and triggers download
+     */
+    public function display() {
+
+        // ensure file download dialog
+        header("Content-type: text/csv");
+        header("Cache-Control: no-store, no-cache");
+        header('Content-Disposition: attachment; filename="filename.csv"');
+
+        // export array to CSV file
+        $out = fopen('php://output', 'w');
+        foreach ($this->array as $fields) {
+            fputcsv($out, $fields);
+        }
+        fclose($out);
+    }
+
 }
 
-
-// export array to CSV file
-$out = fopen('php://output', 'w');
-foreach ($array as $fields) {
-    fputcsv($out, $fields);
-}
-fclose($out);
- 
 ?>
